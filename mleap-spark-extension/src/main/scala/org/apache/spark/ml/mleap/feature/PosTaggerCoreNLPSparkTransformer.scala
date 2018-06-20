e762eece5075c7219d12d6cacf71394c9f2ec858
Charles Flores <cflores@stratio.com>
2018-06-21 00:58:30 +0200

    first integration

---
+++ mleap-spark-extension/src/main/scala/org/apache/spark/ml/mleap/feature/PosTaggerCoreNLPSparkTransformer.scala
@@ -0,0 +1,40 @@
+package org.apache.spark.ml.mleap.feature
+
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.ml.Transformer
+import org.apache.spark.ml.param.ParamMap
+import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, Dataset}
+import ml.combust.mleap.core.feature.PosTaggerCoreNLPModel
+import org.apache.spark.sql.types._
+
+class PosTaggerCoreNLPSparkTransformer (override val uid: String, val model: PosTaggerCoreNLPModel) extends Transformer with HasInputCol with HasOutputCol {
+  def this(model: PosTaggerCoreNLPModel) = this(uid = Identifiable.randomUID("postagger_corenlp"), model = model)
+
+  def setInputCol(value: String): this.type = set(inputCol, value)
+  def setOutputCol(value: String): this.type = set(outputCol, value)
+
+  @org.apache.spark.annotation.Since("2.0.0")
+  override def transform(dataset: Dataset[_]): DataFrame = {
+    val posTaggerCoreNLPUdf = udf {
+      (label: String) => model(label)
+    }
+
+    dataset.withColumn($(outputCol), posTaggerCoreNLPUdf(dataset($(inputCol))))
+  }
+
+  override def copy(extra: ParamMap): Transformer = copyValues(new PosTaggerCoreNLPSparkTransformer(uid, model), extra)
+
+  @DeveloperApi
+  override def transformSchema(schema: StructType): StructType = {
+    require(schema($(inputCol)).dataType.isInstanceOf[StringType],
+      s"Input column must be of type StringType but got ${schema($(inputCol)).dataType}")
+    val inputFields = schema.fields
+    require(!inputFields.exists(_.name == $(outputCol)),
+      s"Output column ${$(outputCol)} already exists.")
+    // TODO fix return schema
+    StructType(schema.fields :+ StructField($(outputCol), DoubleType))
+  }
+}
